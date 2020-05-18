7a6ff6ad80b727c69a2383b1e9d574712e142184
yutaArakawa <y.arakawa71@icloud.com>
2020-05-18 23:12:07 +0900

    add new file ] :q
    ]

---
+++ 2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>課題20</title>
+</head>
+<body>
+<form action="3.php" method="post">
+<input type="text" name="num">
+<input type="submit" value="送信">
+</form>
+</body>
+</html>
---
+++ 3.php
@@ -0,0 +1,13 @@
+<?php
+$num = $_POST['num'];
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>課題20</title>
+</head>
+<body>
+<?php echo $num; ?>
+</body>
+</html>
